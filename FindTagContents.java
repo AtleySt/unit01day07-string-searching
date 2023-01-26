28311cb405da76fa11026a0b14028a22ddee5ed0
Atley St. Vrain <atleystvrain@gmail.com>
2023-01-26 11:54:18 -0600

    Complete | FindTagContents

--- FindTagContents.java
+++ FindTagContents.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class FindTagContents {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please type the HTML you would like to search");
+        String text = scanner.nextLine();
+
+        System.out.println("Now enter the tag you would like to search within");
+        String searchTag = scanner.nextLine();
+
+        int tagPos = text.indexOf("<"+searchTag+">");
+        int textStart = text.indexOf(">", tagPos)+1;
+        int textEnd = text.indexOf("<", textStart);
+
+        System.out.println(text.substring(textStart,textEnd));
     }
 }
